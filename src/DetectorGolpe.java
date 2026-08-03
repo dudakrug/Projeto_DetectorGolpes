df433e68522468d342b71ebc74c154bd0156cd5a
dudakrug <dudakrugamaral@gmail.com>
2026-08-03 17:17:13 -0300

    Classe mensagem criada e logica da classe DetectorGolpe iniciada

--- src/DetectorGolpe.java
+++ src/DetectorGolpe.java
@@ -1,2 +1,35 @@
 public class DetectorGolpe {
+    private int pontuacao;
+    private String classificacao;
+    private String texto;
+    String[] palavrasDoTexto;
+
+    public DetectorGolpe() {
+
+    }
+
+    public void dividirTexto(String texto){
+        String[] palavrasDoTexto =  texto.split(" ");
+    }
+
+    public void definirClassificacao(int pontuacao) {
+        if(pontuacao <= 20){
+            classificacao = "Provavelmente legítma";
+        }
+        else if(pontuacao <= 40) {
+            classificacao = "Suspeita";
+        }
+        else{
+            classificacao = "Provavelmente golpe";
+        }
+    }
+
+    //getters
+    public String getClassificacao() {
+        return classificacao;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
 }
