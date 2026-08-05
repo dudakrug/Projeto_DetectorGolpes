bbf4c1fb0deb31c67ebf196d1ac53b669e226859
lucasvalgoi <lucasluisvalgoi@gmail.com>
2026-08-04 21:34:39 -0300

    Lógica da Classe TextoUtils finalizada

--- src/TextoUtils.java
+++ src/TextoUtils.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,18 +7,40 @@ public class TextoUtils {
     static String entradaUsuario;//mensagem vindo do console pelo user
     static String textoLimpo;//texto de entrada sem pontuação
     static String[] palavrasTexto;
+    static List<String> links = new ArrayList<>();
+    static List<String> palavrasValidas = new ArrayList<>();
 
     static String regex = "(https?://\\S+)|(www\\.\\S+)";
 
+    public static void capturaLink(String mensagem){
+        entradaUsuario = mensagem; //recebe o texto original do usuário
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(entradaUsuario);
+
+        while (matcher.find()) {
+            links.add(matcher.group());
+        }
+        System.out.println("link: " + links);
+    }
 
     public static void recebeMensagem(String mensagem){
         entradaUsuario = mensagem;//entrada do usuário recebe a mensagem como parâmetro
 
+        capturaLink(entradaUsuario);
+
         //texto é limpado, passado pra minúsculo e com pontuação retirada
         textoLimpo = entradaUsuario.toLowerCase().replaceAll("[,!.?]", "");
-        System.out.println(textoLimpo);
+
+        textoLimpo.replaceAll("\\s+", " ");
 
         palavrasTexto = textoLimpo.split(" ");//cada palavra é seperada e vai para lista criada
-        System.out.println(Arrays.toString(palavrasTexto));
+        for (String str : palavrasTexto) {
+            if (str != null && !str.isEmpty()) {
+                palavrasValidas.add(str);
+            }
+        }
+
+        System.out.println("Palavras: " + palavrasValidas);
     }
 }
